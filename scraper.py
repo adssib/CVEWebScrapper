651a57c132c39a6d349cbb18749b2f288fdc0f63
adib <adibakkari@gmail.com>
2025-02-12 15:31:02 -0500

    getting all pages type shit

--- scraper.py
+++ scraper.py
@@ -25,43 +25,51 @@ for x in range (1, 13):
     # the return is the same page as ?page=1 so we can use this behavior to get all pages 
     pageNumber = 1
     firstPageResponse = None
-    baseURL = f"https://www.cvedetails.com/vulnerability-list/year-2024/month-{x}/{months[x-1]}.html?page={pageNumber}&order=1"
-    
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    }
+    while True:
+        baseURL = f"https://www.cvedetails.com/vulnerability-list/year-2024/month-{x}/{months[x-1]}.html?page={pageNumber}&order=1"
+        
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+        }
+        
+        page = requests.get(baseURL, headers=headers)
+        
+        if page.status_code != 200:
+            print(f"Failed to fetch {baseURL}, status code: {page.status_code}")
+            continue
+        if pageNumber == 1:
+            firstPageResponse = page
+        elif firstPageResponse == page:
+            print(f"this page {pageNumber} have the same input as the Frist Page!")
+            break
 
-    page = requests.get(baseURL, headers=headers)
+        time.sleep(1) 
 
-    if page.status_code != 200:
-        print(f"Failed to fetch {baseURL}, status code: {page.status_code}")
-        continue
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    time.sleep(1) 
+        body = soup.find("body")
+        firstDiv = body.find("div", class_="container-xxl p-0 d-flex flex-column justify-content-start")
+        secondDiv = firstDiv.find("div", class_="container-xxl p-0 d-flex justify-content-start")
+        thirdDiv = secondDiv.find(id="contentdiv")
+        forthDiv = thirdDiv.find("div", class_="p-0 pe-2")
+        main = forthDiv.find("main")
+        resultsTableVueDiv = main.find(id="resultsTableVueDiv") 
+        cveSearchresults = resultsTableVueDiv.find(id="searchresults") 
 
-    soup = BeautifulSoup(page.content, "html.parser")
+        if cveSearchresults:
+            allDivCVE = cveSearchresults.find_all("div", class_="border-top py-3 px-2 hover-bg-light")
 
-    body = soup.find("body")
-    firstDiv = body.find("div", class_="container-xxl p-0 d-flex flex-column justify-content-start")
-    secondDiv = firstDiv.find("div", class_="container-xxl p-0 d-flex justify-content-start")
-    thirdDiv = secondDiv.find(id="contentdiv")
-    forthDiv = thirdDiv.find("div", class_="p-0 pe-2")
-    main = forthDiv.find("main")
-    resultsTableVueDiv = main.find(id="resultsTableVueDiv") 
-    cveSearchresults = resultsTableVueDiv.find(id="searchresults") 
+            for singleDivCVE in allDivCVE:
+                row = singleDivCVE.find("div", class_="row") 
+                colMd9 = row.find("div", class_="col-md-9")
+                recondRow = colMd9.find("div", class_="row")
+                h3 = recondRow.find("h3", class_="col-md-4 text-nowrap")
+                link = h3.find("a", href=True) 
+                if link:
+                    href = link['href']
+                    allCVEs.append(href)
+                    print(href + " --> " + baseURL) 
+        else:
+            print(f"Results container not found on page: {baseURL}") 
 
-    if cveSearchresults:
-        allDivCVE = cveSearchresults.find_all("div", class_="border-top py-3 px-2 hover-bg-light")
-
-        for singleDivCVE in allDivCVE:
-            row = singleDivCVE.find("div", class_="row") 
-            colMd9 = row.find("div", class_="col-md-9")
-            recondRow = colMd9.find("div", class_="row")
-            h3 = recondRow.find("h3", class_="col-md-4 text-nowrap")
-            link = h3.find("a", href=True) 
-            if link:
-                href = link['href']
-                allCVEs.append(href)
-                print(href + " --> " + baseURL) 
-    else:
-        print(f"Results container not found on page: {baseURL}") 
+        pageNumber = pageNumber + 1 
