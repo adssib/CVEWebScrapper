b1be660ae938710c704d1bd24a0c2870dd1b9b3e
adib <adibakkari@gmail.com>
2025-02-12 15:11:04 -0500

    small pretesizing

--- scraper.py
+++ scraper.py
@@ -10,6 +10,8 @@ months = [
     "July", "August", "September", "October", "November", "December"
 ]
 
+allCVEs = [] 
+
 # out_dir = "out"
 # os.makedirs(out_dir, exist_ok=True)
 
@@ -18,10 +20,13 @@ for x in range (1, 13):
     # y is the number of pages we are going to loop through  till now we will limit it to 3 and thats it 
     for y in range (1, 3):
         baseURL = f"https://www.cvedetails.com/vulnerability-list/year-2024/month-{x}/{months[x-1]}.html?page={y}&order=1"
+       
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         }
+
         page = requests.get(baseURL, headers=headers)
+
         if page.status_code != 200:
             print(f"Failed to fetch {baseURL}, status code: {page.status_code}")
             continue
@@ -30,9 +35,6 @@ for x in range (1, 13):
 
         soup = BeautifulSoup(page.content, "html.parser")
 
-        # outputFileName = f"_cvedetails_page_{y}_{months[x-1]}_2024.txt"
-        # os.mknod(fileName) 
-        # filePath = os.path.join(out_dir, outputFileName)
         body = soup.find("body")
         firstDiv = body.find("div", class_="container-xxl p-0 d-flex flex-column justify-content-start")
         secondDiv = firstDiv.find("div", class_="container-xxl p-0 d-flex justify-content-start")
@@ -40,7 +42,6 @@ for x in range (1, 13):
         forthDiv = thirdDiv.find("div", class_="p-0 pe-2")
         main = forthDiv.find("main")
         resultsTableVueDiv = main.find(id="resultsTableVueDiv") 
-
         cveSearchresults = resultsTableVueDiv.find(id="searchresults") 
 
         if cveSearchresults:
@@ -54,15 +55,7 @@ for x in range (1, 13):
                 link = h3.find("a", href=True) 
                 if link:
                     href = link['href']
+                    allCVEs.append(href)
                     print(href) 
         else:
-            print(f"Results container not found on page: {baseURL}")
-        # with open(filePath, "w", encoding="utf-8") as f:
-        #     f.write(page.text)
-
-        # print(f"File created: {filePath}")
-
-
-
-# print(page.text)
- 
+            print(f"Results container not found on page: {baseURL}") 
